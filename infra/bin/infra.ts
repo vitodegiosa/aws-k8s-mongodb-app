1dcefbb3dc95fc03321c5b34989aa098d751e42e
Vito De Giosa <vito.degiosa@gmail.com>
2025-07-26 15:28:42 +0200

    stack to setup OIDC for Github Actions

---
+++ infra/bin/infra.ts
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { PolicyDocument, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { GithubActionsOIDCStack } from '../lib/github-actions-oidc-stack';
+import { InfraStack } from '../lib/infra-stack';
+
+const app = new cdk.App();
+const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
+new GithubActionsOIDCStack(app, 'GithubActionsOIDCStack', {
+    env,
+    repositoryConfig: {
+      owner: 'vitodegiosa',
+      repo: 'aws-k8s-mongodb-app',
+    },
+    roleConfig: {
+      inlinePolicies: {
+        AssumeCdkRolePolicy: new PolicyDocument({
+          statements: [new PolicyStatement({
+            actions: ['sts:AssumeRole'],
+            resources: [`arn:aws:iam::${env.account}:role/cdk-*`],
+          })],
+        }),
+        EcrPolicy: new PolicyDocument({
+            statements:[new PolicyStatement({
+                actions: ['ecr:*'],
+                resources: [`arn:aws:ecr:${env.region}:${env.account}:*`]
+            })]
+        })
+      },
+    },
+});
+new InfraStack(app, 'InfraStack', {
+  /* If you don't specify 'env', this stack will be environment-agnostic.
+   * Account/Region-dependent features and context lookups will not work,
+   * but a single synthesized template can be deployed anywhere. */
+
+  /* Uncomment the next line to specialize this stack for the AWS Account
+   * and Region that are implied by the current CLI configuration. */
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+  /* Uncomment the next line if you know exactly what Account and Region you
+   * want to deploy the stack to. */
+  // env: { account: '123456789012', region: 'us-east-1' },
+
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
---
+++ infra/lib/github-actions-oidc-stack.ts
@@ -0,0 +1,83 @@
+import {
+  CfnOutput,
+  Duration,
+  Stack,
+  StackProps,
+  Tags
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import {
+  Conditions,
+  IManagedPolicy,
+  OpenIdConnectProvider,
+  PolicyDocument,
+  Role,
+  WebIdentityPrincipal,
+} from 'aws-cdk-lib/aws-iam';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
+
+export interface GithubActionsRepositoryConfig {
+  readonly owner: string;
+  readonly repo: string;
+  readonly filter?: string;
+}
+
+export interface GithubActionsRoleConfig {
+  readonly inlinePolicies?: Record<string, PolicyDocument>;
+  readonly managedPolicies?: IManagedPolicy[];
+}
+
+export interface GithubActionsOIDCStackProps extends StackProps {
+  readonly repositoryConfig: GithubActionsRepositoryConfig;
+  readonly roleConfig: GithubActionsRoleConfig;
+}
+
+export class GithubActionsOIDCStack extends Stack {
+  constructor(scope: Construct, id: string, props: GithubActionsOIDCStackProps) {
+    super(scope, id, props);
+
+    const githubProvider = new OpenIdConnectProvider(this, 'GithubActionsProvider', {
+      url: 'https://token.actions.githubusercontent.com',
+      clientIds: ['sts.amazonaws.com'],
+    });
+
+    const iamRepoDeployAccess = `repo:${props.repositoryConfig.owner}/${props.repositoryConfig.repo}:${props.repositoryConfig.filter ?? '*'}`
+
+    const conditions: Conditions = {
+      StringLike: {
+        'token.actions.githubusercontent.com:sub': iamRepoDeployAccess,
+      },
+      StringEquals: {
+        'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+      },
+    };
+
+    const role = new Role(this, 'GitHubActionsOidcAccessRole', {
+      assumedBy: new WebIdentityPrincipal(githubProvider.openIdConnectProviderArn, conditions),
+      inlinePolicies: props.roleConfig.inlinePolicies,
+      managedPolicies: props.roleConfig.managedPolicies,
+      roleName: 'GitHubActionsOidcAccessRole',
+      description: 'This role is used via GitHub Actions assume the role in the target AWS account',
+      maxSessionDuration: Duration.hours(12),
+    });
+
+    //Create ECR Repository
+    const repo = new Repository(this, 'SampleAppRepo', {
+      repositoryName: 'sample-app-repo',
+    });
+
+    new CfnOutput(this, 'GitHubActionsOidcAccessRoleArn', {
+      value: role.roleArn,
+      description: `Arn for AWS IAM role with Github Actions OIDC auth for ${iamRepoDeployAccess}`,
+      exportName: 'GitHubActionsOidcAccessRoleArn',
+    });
+
+    new CfnOutput(this, 'ECRRepositoryName', {
+        value: repo.repositoryName,
+        description: 'Name of the ECR Repository to store application docker images',
+        exportName: 'ECRRepositoryName'
+    })
+
+    Tags.of(this).add('component', 'CdkGithubActionsOidcIamRole');
+  }
+}
